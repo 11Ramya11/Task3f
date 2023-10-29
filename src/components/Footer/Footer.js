a3bd5a57192ea75eafb34687d6aee6a01636ac43
11Ramya11 <105539397+11Ramya11@users.noreply.github.com>
2023-10-29 22:30:55 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/khila-7'
+      className='link footer__link'
+    >
+      Created by Me
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,52 @@
+const header = {
+  }
+  
+  const about = {
+    name: 'Ramya',
+    role: 'Computer Science Engineer',
+    description:
+      'An enthusiast, keen at learning things currently pursuing 3rd year B.Tech in VIT-AP University',
+    resume: 'https://drive.google.com/file/d/1KqT2gqltfioUWoYhJKdTqRkACSsh4Dsc/view?usp=drive_link',
+    social: {
+      linkedin: 'www.linkedin.com/in/ramya-bapatla-670b15251',
+      github: 'https://github.com/11Ramya11',
+    },
+  }
+  
+  const projects = [
+   
+    {
+      name: 'Project 1',
+      description:
+        'Weather Monitoring using Arduino',
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+    {
+      name: 'Project 2',
+      description:
+        'Blind Guard using Arduino',
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+  ]
+  
+  const skills = [
+  
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Java',
+    'Python',
+    'React',
+    
+  ]
+
+  
+  const contact = {
+    email: 'ramyabapatal11@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
